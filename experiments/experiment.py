29389ab24868432c78352a5c2eb7646706e350e6
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2024-12-31 16:19:33 -0300

    Removendo variáveis que não estavam sendo utilizadas.

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -6,12 +6,6 @@ from sklearn.svm import OneClassSVM
 from sklearn.metrics import balanced_accuracy_score, accuracy_score
 from cmu import *
 
-cmu: pd.DataFrame = pd.read_csv('datasets/cmu/DSL-StrongPasswordData.csv')
-cmu_training_df = cmu[cmu['sessionIndex'] == 1]
-cmu_test_df = cmu[cmu['sessionIndex'] != 1]
-drop_columns = ['subject', 'sessionIndex', 'rep']
-user_keys: set[str] = set(cmu["subject"].drop_duplicates().tolist())
-
 cmu_database = CMUDatabase('datasets/cmu/DSL-StrongPasswordData.csv')
 
 X_training: dict[str, pd.DataFrame] = {}
@@ -19,18 +13,18 @@ X_test: dict[str, pd.DataFrame] = {}
 y_training: dict[str, list[int]] = {}
 y_test: dict[str, list[int]] = {}
 
-for uk in user_keys:
+for uk in cmu_database.user_keys():
     X_training[uk], y_training[uk] = cmu_database.user_training_rows(uk)
     X_test[uk], y_test[uk] = cmu_database.user_test_rows(uk)
 
 one_class_estimators_map: dict[str, OneClassSVM] = {}
 
-for uk in user_keys:
+for uk in cmu_database.user_keys():
     one_class_estimators_map[uk] = OneClassSVM().fit(X_training[uk], y_training[uk])
 
 user_model_acc_on_genuine_samples_map: dict[str, float] = {}
 
-for uk in user_keys:
+for uk in cmu_database.user_keys():
     predictions = one_class_estimators_map[uk].predict(X_test[uk]).flatten().tolist()
     user_model_acc_on_genuine_samples_map[uk] = accuracy_score(y_test[uk], predictions)
 
@@ -42,7 +36,7 @@ print(f"Acurácia dos modelos One-Vs-One: {average_acc}")
 
 user_model_far_on_attack_samples_map: dict[str, float] = {}
 
-for uk in user_keys:
+for uk in cmu_database.user_keys():
     X_attacks, y_attacks = cmu_database.other_test_rows(uk)
     predictions = one_class_estimators_map[uk].predict(X_attacks).flatten().tolist()
     user_model_far_on_attack_samples_map[uk] = accuracy_score(y_attacks, predictions)
@@ -63,7 +57,7 @@ X_other_test: dict[str, pd.DataFrame] = {}
 y_other_training: dict[str, list[int]] = {}
 y_other_test: dict[str, list[int]] = {}
 
-for uk in user_keys:
+for uk in cmu_database.user_keys():
     X_user_training[uk], y_user_training[uk] = cmu_database.user_training_rows(uk)
     X_other_training[uk], y_other_training[uk] = cmu_database.other_training_rows(uk)
     X_user_test[uk], y_user_test[uk] = cmu_database.user_test_rows(uk)
@@ -72,12 +66,12 @@ for uk in user_keys:
 two_class_estimators_map: dict[str, RandomForestClassifier] = {}
 two_class_acc_map: dict[str, float] = {}
 
-for uk in user_keys:
+for uk in cmu_database.user_keys():
     X = pd.concat([X_user_training[uk], X_other_training[uk]])
     y = y_user_training[uk] + y_other_training[uk]
     two_class_estimators_map[uk] = RandomForestClassifier().fit(X, y)
 
-for uk in user_keys:
+for uk in cmu_database.user_keys():
     X_test = pd.concat([X_user_test[uk], X_other_test[uk]])
     predictions = two_class_estimators_map[uk].predict(X_test)
     y_true = y_user_test[uk] + y_other_test[uk]
