919d5e18ccaae31895345510efbe6eb96f3b54c9
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2025-02-08 15:59:47 -0300

    - Correções no LW; - Excluíndo os hold times dos atributos do CMU para os experimentos com o LW.

--- experimentos/lib/lightweight_alg.py
+++ experimentos/lib/lightweight_alg.py
@@ -1,74 +1,64 @@
-import pandas as pd
-import numpy as np
-
-from sklearn.base import BaseEstimator
 from typing import Self
+
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator
+
 
 class DigraphsMetrics:
     def __init__(self, average: float, std_dev: float, median: float):
         self.average = average
         self.std_dev = std_dev
         self.median = median
+
     average: float
     std_dev: float
     median: float
 
+
 class LightWeightAlg(BaseEstimator):
-    a: float
-    b: float
     threshold: float
 
     _digraphs_metrics: dict[str, DigraphsMetrics] = {}
 
-    def __init__(self, a: float = .95, b: float = 1.05, threshold: float = .7):
-        self.a = a
-        self.b = b
+    def __init__(self, threshold: float = .7):
         self.threshold = threshold
-    
+
     def get_params(self, deep: bool = True) -> dict[str, float]:
         return {
-            'a': self.a,
-            'b': self.b,
             'threshold': self.threshold
         }
-    
-    def set_params(self, **params) -> Self:
-        return LightWeightAlg(params['a'], params['b'], params['threshold'])
 
-    def fit(self, X: pd.DataFrame, y: list[int] | None = None):
-        for digraph_name, values in X.items():
-            average = values.mean() 
+    def set_params(self, **params) -> Self:
+        return LightWeightAlg(params['threshold'])
+
+    def fit(self, x: pd.DataFrame, y: list[int] | None = None):
+        for digraph_name, values in x.items():
+            average = values.mean()
             std_dev = values.std()
             median = values.median()
             self._digraphs_metrics[digraph_name] = DigraphsMetrics(average, std_dev, median)
-        time_spend_per_sample = []
-        for row in X.iterrows():
-            time_spend_per_sample.append(row[1].sum())
-        self.is_fitted = True
 
-    def predict(self, X: pd.DataFrame):
+    def predict(self, x: pd.DataFrame):
         digraphs_hits_matrix: list[list[bool]] = []
         results: list[int] = []
-        
-        for _, vec in X.iterrows():
+
+        for _, vec in x.iterrows():
             digraphs_hits_vec = []
             for digraph_name, value in vec.items():
                 digraph_metrics = self._digraphs_metrics[digraph_name]
                 av = digraph_metrics.average
                 std = digraph_metrics.std_dev
                 md = digraph_metrics.median
-                res = min(av, md) * (self.a - std / av) <= value 
-                res = res and value <= max(av, md) * (self.b + std / av)
+                res = (min(av, md) * (0.95 - std / av) <= value) and (value <= max(av, md) * (1.05 + std / av))
                 digraphs_hits_vec.append(res)
             digraphs_hits_matrix.append(digraphs_hits_vec)
 
+        l = len(digraphs_hits_matrix[0])
         for pred in digraphs_hits_matrix:
-            s: float = 1
-            l = len(pred)
-            for i in range(2, l):
+            s: float = 1 if pred[0] else 0
+            for i in range(1, l):
                 s += 1.5 if pred[i - 1] else 1
-            results.append(1 if s / (9 * 1.5 + 1) >= 0.7 else -1) 
-        
+            results.append(1 if s / ((l - 1) * 1.5 + 1) >= 0.7 else -1)
+
         return np.array(results)
-
-
--- experimentos/one_class_lw.py
+++ experimentos/one_class_lw.py
@@ -1,14 +1,18 @@
 import os
+
 from lib.cmu_dataset import CMUDataset
-from lib.runners.single_class_experiment_runner import SingleClassExperimentRunner
 from lib.lightweight_alg import LightWeightAlg
-from lib.utils import lw_split, save_results
+from lib.runners.single_class_experiment_runner import SingleClassExperimentRunner
+from lib.utils import lw_split, first_session_split, save_results
+
 
 def main() -> None:
-    cmu_database = CMUDataset('datasets/cmu/DSL-StrongPasswordData.csv', lw_split, "DD.*")
+    cmu_database = CMUDataset('datasets/cmu/DSL-StrongPasswordData.csv',
+                              first_session_split, "(DD|UD)(\\.[A-Za-z]+)+")
     one_class_lw_experiment = SingleClassExperimentRunner(dataset=cmu_database, estimator=LightWeightAlg())
     results = one_class_lw_experiment.exec()
     save_results(os.path.basename(__file__), results.to_dict())
+
 
 if __name__ == "__main__":
     main()
