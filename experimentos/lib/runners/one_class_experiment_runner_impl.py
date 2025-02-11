68e40662e37d94353c624b1ae869652334d7e4ae
Thiago F. Dias <t.dias@mshimizu.com.br>
2025-02-11 12:28:24 -0300

    - Correções de formatação.

--- experimentos/lib/runners/one_class_experiment_runner_impl.py
+++ experimentos/lib/runners/one_class_experiment_runner_impl.py
@@ -14,5 +14,7 @@ class OneClassExperimentRunnerImpl(OneClassExperimentRunner):
         self._one_class_estimators_hp_map['default'] = self._estimator.get_params()
         for uk in self._dataset.user_keys():
             self._estimator.fit(self._X_training[uk], self._y_training[uk])
-            self._predictions_on_genuine_samples_map[uk] = self._estimator.predict(self._X_genuine_test[uk]).flatten().tolist()
-            self._predictions_on_attacks_samples_map[uk] = self._estimator.predict(self._X_impostors_test[uk]).flatten().tolist()
+            self._predictions_on_genuine_samples_map[uk] = \
+                self._estimator.predict(self._X_genuine_test[uk]).flatten().tolist()
+            self._predictions_on_attacks_samples_map[uk] = \
+                self._estimator.predict(self._X_impostors_test[uk]).flatten().tolist()
