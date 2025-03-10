a17a630d57aa35b8419db0ae1ff2bf3dc7d6227d
Thiago F. Dias <t.dias@mshimizu.com.br>
2025-03-10 14:49:38 -0300

    - Resultados; - Teste em thresholds fixados.

--- experimentos/one_class_lw_threshold_tuning.py
+++ experimentos/one_class_lw_threshold_tuning.py
@@ -7,7 +7,7 @@ from lib.cmu_dataset import CMUDataset
 from lib.lightweight_alg import LightWeightAlg
 from lib.one_class_threshold_search_cv import OneClassThresholdSearchCV
 from lib.runners.one_class_experiment_with_treshold_search_cv import OneClassExperimentWithTresholdSearchCV
-from lib.utils import lw_split, save_results, first_session_split
+from lib.utils import lw_split, save_results, first_session_split, float_range
 
 
 def main() -> None:
@@ -16,7 +16,7 @@ def main() -> None:
     one_class_lw_gs = OneClassThresholdSearchCV(
         estimator=LightWeightAlg(),
         cv=one_class_lw_grid_cv,
-        thresholds=np.linspace(0.3, 0.65, 50),
+        thresholds=float_range(0.2, 0.7, 0.025),
     )
     one_class_lw_with_hpo_experiment = OneClassExperimentWithTresholdSearchCV(
         dataset=cmu_database, estimator=one_class_lw_gs
