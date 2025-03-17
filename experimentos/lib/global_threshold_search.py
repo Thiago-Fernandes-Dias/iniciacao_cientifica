dc3458e95d6b990123766e335c72e0a97e2c6a08
Thiago F. Dias <t.dias@mshimizu.com.br>
2025-03-17 17:22:37 -0300

    Finalizando experimento com ajuste global do threshold.

--- experimentos/lib/global_threshold_search.py
+++ experimentos/lib/global_threshold_search.py
@@ -1,10 +1,10 @@
 ﻿from typing import Callable
 
+import numpy as np
 from sklearn.base import BaseEstimator
+from sklearn.metrics import accuracy_score
 
 from lib.cmu_dataset import CMUDataset
-from lib.lightweight_alg import LightWeightAlg
-from lib.runners.one_class_experiment_runner_impl import OneClassExperimentRunnerImpl
 
 
 class GlobalThresholdTuning:
@@ -20,26 +20,28 @@ class GlobalThresholdTuning:
         self._thresholds = thresholds
 
     def search(self) -> float:
-        self._fit_estimators()
         best_threshold: float = 0
         best_bacc: float = 0
+        self._fit_estimators()
         for threshold in self._thresholds:
-            self._estimators_map[uk] = self._estimators_map[uk].set_params(**{"threshold": threshold})
-            far_map: dict[str, float] = {}
-            frr_map: dict[str, float] = {}
+            bacc_map: dict[str, float] = {}
             for uk in self._dataset.user_keys():
+                self._estimators_map[uk] = self._estimators_map[uk].set_params(**{"threshold": threshold})
                 x_g_test, y_g_test = self._dataset.user_test_set(uk)
                 x_i_test, y_i_test = self._dataset.impostors_test_set(uk)
-                g_pred = self._estimators_map[uk].predict(x_i_test)
-                i_pred = self._estimators_map[uk].set_predict_request()
-            bacc = (g_recall + i_recall) / 2
-            if bacc > best_bacc:
+                g_pred = self._estimators_map[uk].predict(x_g_test)
+                i_pred = self._estimators_map[uk].predict(x_i_test)
+                g_recall = accuracy_score(y_g_test, g_pred)
+                i_recall = accuracy_score(y_i_test, i_pred)
+                bacc_map[uk] = (g_recall + i_recall) / 2
+            av_bacc = np.average(list(bacc_map.values())).item()
+            if av_bacc > best_bacc:
                 best_threshold = threshold
-                best_bacc = bacc
+                best_bacc = av_bacc
         return best_threshold
 
     def _fit_estimators(self) -> None:
         for uk in self._dataset.user_keys():
             self._estimators_map[uk] = self._estimator_factory()
-            x_g_training, y_g_training, x_i_training, y_i_training = self._dataset.two_class_training_set(uk)
+            x_g_training, y_g_training = self._dataset.one_class_training_set(uk)
             self._estimators_map[uk].fit(x_g_training, y_g_training)
