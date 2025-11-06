c21a7161f1f739965771574ce57309c2ec1571a6
Thiago F. Dias <t.dias@mshimizu.com.br>
2025-11-06 09:38:29 -0300

    - Reorganização das pastas; - Versão final do relatório da IC de 2024.

--- Makefile
+++
@@ -1,2 +0,0 @@
-run_exp:
-	@python experimentos/main.py
--- experimentos/print_default_values.py
+++
@@ -1,8 +0,0 @@
-from lib.repositories.results_repository_factory import results_repository_factory
-from icecream import ic
-
-repo = results_repository_factory()
-
-results = repo.read_results("SVM (Keyrecs)")
-
-ic(results.hp_per_seed[0])
