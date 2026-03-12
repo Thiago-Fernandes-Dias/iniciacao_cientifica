7fa69599723d4883c2520bce37c1bb47a1e78622
Thiago F. Dias <t.dias@mshimizu.com.br>
2026-03-12 16:56:10 -0300

    relatório parcial.

--- 2025/experimentos/tex_fvc_2004.py
+++ 2025/experimentos/tex_fvc_2004.py
@@ -1,51 +1,46 @@
 from flx.benchmarks.matchers import CosineSimilarityMatcher
-from flx.data.dataset import Dataset, Identifier, IdentifierSet
+from flx.data.dataset import Dataset
 from flx.data.image_loader import FVC2004Loader
-from flx.data.label_index import LabelIndex
-from flx.extractor.fixed_length_extractor import DeepPrintExtractor, get_DeepPrint_Tex
+from flx.extractor.fixed_length_extractor import DeepPrintExtractor
 from flx.scripts.generate_benchmarks import create_verification_benchmark
 
-NUM_IMPRESSIONS = 8
+from flx.data.transformed_image_loader import TransformedImageLoader
+from flx.image_processing.binarization import LazilyAllocatedBinarizer
+from flx.data.image_helpers import pad_and_resize_to_deepprint_input_size
+
+from flx.extractor.fixed_length_extractor import get_DeepPrint_TexMinu
 
 
 def main():
-    training_ids = IdentifierSet(
-        [Identifier(s, i) for s in range(100) for i in range(8)]
+    # modelo pré-treinado disponibilizado no repositório
+    deep_print_tex_extractor: DeepPrintExtractor = get_DeepPrint_TexMinu(
+        num_training_subjects=8000, num_dims=256
     )
-
-    deep_print_tex_extractor: DeepPrintExtractor = get_DeepPrint_Tex(
-        num_training_subjects=training_ids.num_subjects, num_texture_dims=256
-    )
-
-    training_dataset = Dataset(FVC2004Loader("D:\Fingerprints_dataset\FVC\FVC2004\Dbs\DB1_A"), training_ids)
-
-    label_dataset = Dataset(LabelIndex(training_ids), training_ids)
-
-    deep_print_tex_extractor.fit(
-        fingerprints=training_dataset,
-        minutia_maps=None,
-        labels=label_dataset,
-        validation_fingerprints=None,
-        validation_benchmark=None,
-        num_epochs=100,
-        out_dir="./models",
-    )
-
     deep_print_tex_extractor.load_best_model("./models")
 
-    test_dataset = Dataset(
-        FVC2004Loader("D:\Fingerprints_dataset\FVC\FVC2004\Dbs\DB1_B"),
-        IdentifierSet([Identifier(s, i) for s in range(101, 110) for i in range(8)]),
+    image_loader = TransformedImageLoader(
+        images=FVC2004Loader(r"FVC2004\Dbs\DB1_B"),
+        poses=None,
+        transforms=[
+            LazilyAllocatedBinarizer(5.0),
+            pad_and_resize_to_deepprint_input_size,
+        ],
     )
 
-    tex_embeddings, _ = deep_print_tex_extractor.extract(test_dataset)
+    test_dataset: Dataset = Dataset(image_loader, image_loader.ids)
+    tex_embeddings, minutia_embeddings = deep_print_tex_extractor.extract(test_dataset)
 
     benchmark = create_verification_benchmark(
-        subjects=list(range(101, 110)),
-        impressions_per_subject=list(range(6, 8)),
+        subjects=list(range(100, 110)),
+        impressions_per_subject=list(range(0, 8)),
     )
 
-    matcher = CosineSimilarityMatcher(tex_embeddings)
+    from flx.data.embedding_loader import EmbeddingLoader
+
+    matcher = CosineSimilarityMatcher(
+        EmbeddingLoader.combine(tex_embeddings, minutia_embeddings)
+    )
+    # matcher = CosineSimilarityMatcher(tex_embeddings) - tbm funciona, mas a EER aumentou um pouco nos testes que fiz
 
     results = benchmark.run(matcher)
 
@@ -53,5 +48,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # freeze_support()
     main()
